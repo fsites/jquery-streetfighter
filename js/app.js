55c1f8e666f2490a53f0931eb09f7aa3b7e7f93e
fsites <fred.sites@yahoo.com>
2015-01-04 17:43:36 -0800

    jquery project.. x button works to press and hold for cool pose

---
+++ js/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+	
+	$('.ryu').mouseenter(function () {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function () {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function () {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+		);
+	})
+	.mouseup(function () {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	})
+
+
+});
+
+function playHadouken() {
+		$('#hadouken-sound') [0].volume = .5;
+		$('#hadouken-sound') [0].load();
+		$('#hadouken-sound') [0].play();
+}
+
+$(document).keydown(function(x) {
+	if (x.keyCode === 88) {
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-cool').show();
+	}	
+})
+	.keyup(function(x) {
+		if (x.keyCode === 88) {
+		$('.ryu-cool').hide();
+		$('.ryu-ready').show();
+		}
+});
